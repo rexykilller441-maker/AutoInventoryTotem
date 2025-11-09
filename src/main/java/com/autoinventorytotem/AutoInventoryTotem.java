40f186f219d7d4af4196f8715adb05464208ee0b
rexykilller441-maker <rexykilller441@gmail.com>
2025-11-09 15:13:33 +0530

    Update AutoInventoryTotem.java

--- src/main/java/com/autoinventorytotem/AutoInventoryTotem.java
+++ src/main/java/com/autoinventorytotem/AutoInventoryTotem.java
@@ -1,23 +1,37 @@
 package com.autoinventorytotem;
 
-import com.autoinventorytotem.config.ModConfig;
-import com.autoinventorytotem.handler.InventoryHandler;
-import com.autoinventorytotem.handler.TickHandler;
-import me.shedaniel.autoconfig.AutoConfig;
-import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.MinecraftClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@Environment(EnvType.CLIENT)
 public class AutoInventoryTotem implements ClientModInitializer {
-    public static final String MODID = "autoinventorytotem";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+    public static final String MOD_ID = "autoinventorytotem";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitializeClient() {
-        AutoConfig.register(ModConfig.class, JanksonConfigSerializer::new);
-        InventoryHandler.init();
+        ModConfigRegister.register();
         TickHandler.register();
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            try {
+                if (client.player != null && client.world != null) {
+                    TickHandler.onTick(client);
+                }
+            } catch (Throwable t) {
+                LOGGER.error("Error during client tick", t);
+            }
+        });
+
         LOGGER.info("AutoInventoryTotem initialized");
+    }
+
+    public static MinecraftClient getClient() {
+        return MinecraftClient.getInstance();
     }
 }
